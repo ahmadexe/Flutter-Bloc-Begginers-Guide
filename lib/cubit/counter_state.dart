7fda3a73b0fa0468c7380e72d38ede82f6c10936
Ahmad <muahmad710@gmail.com>
2022-08-31 18:35:06 +0500

    Using bloc provider and bloc builder

---
+++ lib/cubit/counter_state.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'counter_cubit.dart';
+
+class CounterState {
+  int counterValue;
+  CounterState({
+    required this.counterValue,
+  });
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:bloc_tutorial/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "This is the current counter",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state) {
+                return Text(state.counterValue.toString(), style: TextStyle(fontSize: 20));
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increment();
+                    },
+                    child: Icon(Icons.add)),
+                ElevatedButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                    },
+                    child: Icon(Icons.remove))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
